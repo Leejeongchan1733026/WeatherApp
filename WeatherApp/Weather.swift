02dc74766cd5d770af7001dda2517856ba7758ee
Leejeongchan1733026 <cksqls288@gmail.com>
2022-06-11 14:05:24 +0900

    openWeather AIP 설정 기본

---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherData.swift
+//  WeatherApp
+//
+//  Created by 이정찬 on 2022/06/11.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let temp_min: Double
+    let temp_max: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
